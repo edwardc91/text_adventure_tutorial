201df862d943322377fa42c16aea0f489f046aa2
Eduardo Miguel Hernández <32711490+edwardc91@users.noreply.github.com>
2024-02-01 15:12:51 -0500

    Added a max remembered lines to player input history

--- main.gd
+++ main.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@export var max_input_remembered: int = 30
 @export var input_response_scene: PackedScene = preload("res://components/ui/input_response.tscn")
 
 @onready var _history_rows: VBoxContainer = %HistoryRows
@@ -16,13 +17,19 @@ func _process(delta: float) -> void:
 
 
 func _on__history_scroll_changed() -> void:
-	_history_scroll.set_v_scroll(_history_scrollbar.max_value)
+	_history_scroll.set_v_scroll(int(_history_scrollbar.max_value))
 
 
 func _on_input_text_submitted(new_text: String) -> void:
-	if not new_text.is_empty():
-		var new_input_response = input_response_scene.instantiate()
-		new_input_response.update_texts(
-			new_text, "User input at " + Time.get_datetime_string_from_system()
-		)
-		_history_rows.add_child(new_input_response)
+	if new_text.is_empty():
+		return
+
+	var new_input_response = input_response_scene.instantiate()
+	new_input_response.update_texts(
+		new_text, "User input at " + Time.get_datetime_string_from_system()
+	)
+
+	_history_rows.add_child(new_input_response)
+
+	if _history_rows.get_child_count() >  max_input_remembered:
+		_history_rows.get_child(0).queue_free()
